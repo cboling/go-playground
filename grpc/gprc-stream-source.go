f54b12789e8dd884df13c994df1090efa91ad79f
Chip Boling <chip@bcsw.net>
2018-09-09 18:10:04 -0500

    Start filling out of bi-directional / channel support example for gRPC

--- grpc/gprc-stream-source.go
+++ grpc/gprc-stream-source.go
@@ -23,18 +23,19 @@ import (
 	"github.com/cboling/go-playground/grpc/example"
 	"google.golang.org/grpc"
 	"log"
+	"math/rand"
 	"time"
 )
 
 const (
-	address = "localhost:50051"
+	port = ":50051"
 )
 
 // This does a unary request/response operation
 func unaryOperation(client example.WorkerClient) {
 	fmt.Println(client)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	// Create request to send
@@ -71,4 +72,51 @@ func main() {
 
 	// Try the unary operation
 	unaryOperation(c)
+
+	// If here, the simple round trip worked.  Now lets create a
+	// request generator that will periodically send out requests
+	// to the consumer which will respond on it's own when it is ready
+	//
+	// After a maximum number of requests, a shutdown is sent to have
+	// the consumer abort any outstanding responces
+	maxMessages := 100
+	generator := requestGenerator(maxMessages, 1000*time.Millisecond)
+
+	go func(gen chan<- example.ExampleRequest) {
+		for request := range gen {
+			go func(req example.ExampleRequest) {
+				// TODO: Send received messages to consumer
+				time.Sleep(5 * time.Millisecond)
+			}(request)
+		}
+		// TODO: Send the shutdown message
+	}(generator)
+
+	// Now receive the responses
+	//responseReceiver := make(chan<- example.ExampleResponse)
+	for {
+		// TODO: Receive the responses here  (use a buffered channel)
+		time.Sleep(5 * time.Second)
+		// responseReceiver <-
+	}
+}
+
+func requestGenerator(max int, interMessageDelay time.Duration) chan<- example.ExampleRequest {
+	output := make(chan example.ExampleRequest)
+
+	go func(maxRequests int) {
+		for maxRequests > 0 {
+			time.Sleep(interMessageDelay)
+
+			request := example.ExampleRequest{
+				PonId: uint32(rand.Int31n(16)),
+				OnuId: uint32(rand.Int31n(128)),
+			}
+			output <- request
+			maxRequests--
+		}
+		close(output)
+	}(max)
+
+	return output
 }
--- grpc/grpc-stream-consumer.go
+++ grpc/grpc-stream-consumer.go
@@ -17,6 +17,53 @@
 
 package main
 
-func main() {
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
 
+	"github.com/cboling/go-playground/grpc/example"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+// server is used to implement example.WorkerServer
+type server struct{}
+
+// RequestUnaryOperation implements example.WorkerServer unary response operation
+func (s *server) RequestUnaryOperation(ctx context.Context, in *example.ExampleRequest) (*example.ExampleResponse, error) {
+
+	// Swap the data in the payload and send it back
+
+	myData := make([]byte, len(in.Payload))
+
+	for val := 0; val < len(myData); val++ {
+		myData[val] = in.Payload[len(myData)-val-1]
+	}
+	response := example.ExampleResponse{
+		UtcTimestamp: in.UtcTimestamp,
+		PonId:        in.PonId,
+		OnuId:        in.OnuId,
+		Payload:      myData,
+	}
+	return &response, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	example.RegisterWorkerServer(s, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
