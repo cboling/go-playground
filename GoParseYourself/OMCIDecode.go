29b72d5944cf56908785243c95f22ff7f76f5d10
Chip Boling <chip.boling@adtran.com>
2018-10-11 15:03:28 -0500

    omci anyone?

---
+++ GoParseYourself/OMCIDecode.go
@@ -0,0 +1,16 @@
+package GoParseYourself
+
+// TODO: Will focus on creation of an OMCI Frame decoder  (separate one for encode).
+
+// TODO: Look into the go 'encoding' interfaces  -> import "encoding" and derive from BinaryMarshaller
+//       and BinaryUnmarshaler
+
+// TODO: Leverage design patterns from the 'gopacket:  https://godoc.org/github.com/google/gopacket#pkg-examples' library
+
+// TODO: Take a look also at: https://github.com/ghedo/go.pkt
+
+// TODO: Read over Brad Israel's blog article:  http://www.bisrael8191.com/Go-Packet-Sniffer/
+
+func main() {
+	
+}
